294b6d12ba9e07d435bb2afe6767afa1a91785e4
Danny Fuhriman <danny.fuhriman@Okta.com>
2024-05-01 13:10:45 -0700

    add config to control MFA login

--- src/components/navigation/desktop/nav-bar-buttons.tsx
+++ src/components/navigation/desktop/nav-bar-buttons.tsx
@@ -3,8 +3,12 @@ import Typography from '@mui/material/Typography';
 import { LoginButton, LogoutButton } from 'components';
 import { SignupButton } from '../../Buttons/signup-button';
 
+const { VITE_MFA_ENABLED: MFA_ENABLED } = import.meta.env;
+
 export const NavBarButtons = () => {
 	const { isAuthenticated } = useAuth0();
+
+	const mfaEnabled = MFA_ENABLED == 'true' || false;
 
 	return (
 		<div className='nav-bar__buttons'>
@@ -12,12 +16,14 @@ export const NavBarButtons = () => {
 				<>
 					<SignupButton />
 					<LoginButton />
-					<LoginButton variant='mfa' sx={{ flexWrap: 'wrap' }}>
-						<div>Login</div>
-						<Typography variant='subtitle1' color='white'>
-							w/ MFA
-						</Typography>
-					</LoginButton>
+					{mfaEnabled && (
+						<LoginButton variant='mfa' sx={{ flexWrap: 'wrap' }}>
+							<div>Login</div>
+							<Typography variant='subtitle1' color='white'>
+								w/ MFA
+							</Typography>
+						</LoginButton>
+					)}
 				</>
 			)}
 			{isAuthenticated && (
